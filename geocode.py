a95265fcac22b692eb0f40572498282dda331162
victd <55512729+victd@users.noreply.github.com>
2022-11-25 16:31:50 -0500

    Update geocode.py

--- geocode.py
+++ geocode.py
@@ -21,6 +21,7 @@ from datetime import datetime
 # want to steer clear of American Thanksgiving
 
 # update, still needed to install runtimeInstaller in order for the maps to load
+# when doing Route Modelling, the webView2 runs 6 instances with 1 instance of Trux open
 
 
 gmaps = googlemaps.Client(key='Add Your Key here')
