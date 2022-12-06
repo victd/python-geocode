7d7ae8901616ecdecbeed4f81e316ca3acb4ddea
victd <55512729+victd@users.noreply.github.com>
2022-12-06 09:36:48 -0500

    Update geocode.py

--- geocode.py
+++ geocode.py
@@ -23,6 +23,7 @@ from datetime import datetime
 # update, still needed to install runtimeInstaller in order for the maps to load
 # when doing Route Modelling, the webView2 runs 6 instances with 1 instance of Trux open
 # will need to open a ticket concerning performance, no response but many users reporting the same issue
+# same with Google maps after the upgrade, portrait
 
 
 gmaps = googlemaps.Client(key='Add Your Key here')
